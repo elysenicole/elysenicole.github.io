62be2d14637517f3a9ee767df39ec6fd6fe008a2
elysenicole <enave1@lsu.edu>
2026-04-21 15:35:18 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -12,3 +12,10 @@ var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
   L.geoJSON(data).addTo(map);
 });
+var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
+var radarDisplayOptions = {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true
+};
+var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
